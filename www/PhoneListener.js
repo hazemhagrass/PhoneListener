8add184ff6659d11235cc92e53d1c690fc08ef24
Hazem Hagrass <hazem.hagrass@badrit.com>
2015-08-26 16:56:45 +0300

    Fix define

--- www/PhoneListener.js
+++ www/PhoneListener.js
@@ -1,23 +1,19 @@
-cordova.define("com.badrit.PhoneListener.PhoneListener", function(require, exports, module) { /**
- * @constructor
- */
 var PhoneListener = function() {};
 
-	PhoneListener.prototype.monitorSignal = function(success, failure) {
-		cordova.exec(success, failure, "PhoneListener", "monitor_signal", []);
+PhoneListener.prototype.monitorSignal = function(success, failure) {
+	cordova.exec(success, failure, "PhoneListener", "monitor_signal", []);
+};
+
+
+// Plug in to Cordova
+cordova.addConstructor(function() {
+
+	if (!window.Cordova) {
+		window.Cordova = cordova;
 	};
 
 
-	// Plug in to Cordova
-	cordova.addConstructor(function() {
+	if (!window.plugins) window.plugins = {};
+	window.plugins.PhoneListener = new PhoneListener();
 
-		if (!window.Cordova) {
-			window.Cordova = cordova;
-		};
-
-
-		if (!window.plugins) window.plugins = {};
-		window.plugins.PhoneListener = new PhoneListener();
-
-	});
 });
