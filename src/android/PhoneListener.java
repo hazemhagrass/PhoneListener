50ed40c3c011149fdfb34bfd51aa660552e6fe57
Hazem Hagrass <hazem.hagrass@badrit.com>
2015-08-26 18:02:30 +0300

    Call js every time you got change

--- src/android/PhoneListener.java
+++ src/android/PhoneListener.java
@@ -1,15 +1,6 @@
 package com.badrit.PhoneListener;
 
-import android.app.Activity;
-import android.content.ContentUris;
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.Intents;
-import android.provider.ContactsContract.PhoneLookup;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
@@ -20,20 +11,13 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class PhoneListener extends CordovaPlugin {
 
     private Context context;
     private CallbackContext callbackContext;
+    private JSONObject data;
 
     @Override
     public boolean execute(String action, JSONArray data,
@@ -44,12 +28,12 @@ public class PhoneListener extends CordovaPlugin {
         if (action.equals("monitor_signal")) {
             monitorSignal();
 
-            PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
+            PluginResult r = new PluginResult(PluginResult.Status.OK, data);
             r.setKeepCallback(true);
             callbackContext.sendPluginResult(r);
         }
 
-        return false;
+        return true;
     }
 
     private void monitorSignal(){
@@ -65,7 +49,7 @@ public class PhoneListener extends CordovaPlugin {
                 super.onSignalStrengthsChanged(signalStrength);
 
                 try {
-                    JSONObject data = new JSONObject();
+                    data = new JSONObject();
                     data.put("cdma_strength", cdmaStrength);
                     data.put("cdma_dbm", signalStrength.getCdmaDbm());
                     data.put("cdma_ecio", signalStrength.getCdmaEcio());
@@ -76,7 +60,10 @@ public class PhoneListener extends CordovaPlugin {
                     data.put("gsm_signal_strength", signalStrength.getGsmSignalStrength());
 
                     Log.d("PhoneStateListener", data.toString());
-                    callbackContext.success(data);
+                    PluginResult r = new PluginResult(PluginResult.Status.OK, data);
+                    r.setKeepCallback(true);
+                    callbackContext.sendPluginResult(r);
+
                 } catch (Exception e) {
                     Log.v("ContactPicker", "Parsing contact failed: " + e.getMessage());
                     callbackContext.error("Parsing contact failed: " + e.getMessage());
