2ddfe19e6ec33d361a66948045d94a2deb7a519e
Hazem Hagrass <hazem.hagrass@badrit.com>
2015-08-26 13:24:33 +0300

    Retrieve Signal Strength

--- src/android/PhoneListener.java
+++ src/android/PhoneListener.java
@@ -10,7 +10,11 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Intents;
 import android.provider.ContactsContract.PhoneLookup;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -37,8 +41,55 @@ public class PhoneListener extends CordovaPlugin {
         this.callbackContext = callbackContext;
         this.context = cordova.getActivity().getApplicationContext();
 
+        if (action.equals("monitor_signal")) {
+            monitorSignal();
+
+            PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
+            r.setKeepCallback(true);
+            callbackContext.sendPluginResult(r);
+        }
 
         return false;
     }
 
+    private void monitorSignal(){
+        TelephonyManager phonyManager  = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        PhoneStateListener mListener = new PhoneStateListener(){
+            @Override
+            public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+
+                int strengthAmplitude = signalStrength.getCdmaDbm();
+                String cdmaStrength = String.valueOf(strengthAmplitude);
+
+                super.onSignalStrengthsChanged(signalStrength);
+
+                try {
+                    JSONObject data = new JSONObject();
+                    data.put("cdma_strength", cdmaStrength);
+                    data.put("cdma_dbm", signalStrength.getCdmaDbm());
+                    data.put("cdma_ecio", signalStrength.getCdmaEcio());
+                    data.put("evo_dbn", signalStrength.getEvdoDbm());
+                    data.put("evo_ecio", signalStrength.getEvdoEcio());
+                    data.put("evo_snr", signalStrength.getEvdoSnr());
+                    data.put("gsm_bit_error_rate", signalStrength.getGsmBitErrorRate());
+                    data.put("gsm_signal_strength", signalStrength.getGsmSignalStrength());
+
+                    Log.d("PhoneStateListener", data.toString());
+                    callbackContext.success(data);
+                } catch (Exception e) {
+                    Log.v("ContactPicker", "Parsing contact failed: " + e.getMessage());
+                    callbackContext.error("Parsing contact failed: " + e.getMessage());
+                }
+
+            }
+        };
+        phonyManager.listen(mListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+        Toast.makeText(context, "Monitoring Start", Toast.LENGTH_SHORT).show();
+    }
+
+    public void getNetworkSignalStrength()
+    {
+
+    }
 }
--- www/PhoneListener.js
+++ www/PhoneListener.js
@@ -1,20 +1,21 @@
-/**
+cordova.define("com.badrit.PhoneListener.PhoneListener", function(require, exports, module) { /**
  * @constructor
  */
 var PhoneListener = function() {};
 
-PhoneListener.prototype.monitor = function(success, failure) {
-	cordova.exec(function() {}, failure, "PhoneListener", "monitor", []);
-};
-
-// Plug in to Cordova
-cordova.addConstructor(function() {
-
-	if (!window.Cordova) {
-		window.Cordova = cordova;
+	PhoneListener.prototype.monitorSignal = function(success, failure) {
+		cordova.exec(success, failure, "PhoneListener", "monitorSignal", []);
 	};
 
+// Plug in to Cordova
+	cordova.addConstructor(function() {
 
-	if (!window.plugins) window.plugins = {};
-	window.plugins.PhoneListener = new PhoneListener();
+		if (!window.Cordova) {
+			window.Cordova = cordova;
+		};
+
+
+		if (!window.plugins) window.plugins = {};
+		window.plugins.PhoneListener = new PhoneListener();
+	});
 });
